25d096b35a6e4300bff82b309b8ae2be8e062dc8
Matt Sell <matt.sell@improvingenterprises.com>
2014-08-17 20:00:33 -0500

    initial commit

---
+++ AgileFitness.API/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Microsoft.Owin;
+using Owin;
+
+namespace AgileFitness.API
+{
+    [assembly: OwinStartup(typeof(AgileFitness.API.Startup))]
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            app.UseWebApi(config);
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Agile Fitness
+
+## The primary goal of this project is to demonstrate softare patterns.
+- The backend will be comprised of Asp.net web api 2, Owin, and Identity.
+- The front end will be AngularJS
+- HighwayData will be used for data access over EF
+
+Reference
+http://bitoftech.net/2014/06/01/token-based-authentication-asp-net-web-api-2-owin-asp-net-identity/
